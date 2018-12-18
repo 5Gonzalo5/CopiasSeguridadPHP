488893664045ccf3ee32d41302dcb58a9c720589
ASIR <ASIR@PC5041>
2018-12-18 13:42:24 +0100

    Es el cuarto commit

--- index.php
+++ index.php
@@ -1,25 +1,6 @@
 <?php
-echo '<!doctype html>
-<html lang="es">
-<head>
-   <meta charset="utf-8">
-   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-   <meta name="viewport" content="width=device-width, initial-scale=1">
-   <link rel="stylesheet" href="cssmenu/styles.css">
-   <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
-   <script src="script.js"></script>
-   <title>Copias de Seguridad</title>
-</head>
-<body>
 
-<div id="cssmenu">
-<ul>
-   <li class="active"><a href="#"><span>Home</span></a></li>
-   <li><a href="RealizarCopia.php"><span>Realizar Cop.Seg.</span></a></li>
-   <li><a href="RestaurarCopia.php"><span>Restaurar Cop.Seg.</span></a></li>
-   <li class="last"><a href="#"><span>Exit</span></a></li>
-</ul>
-</div>
+include_once 'header.php';
 
-</body>
-<html>';
+
+include_once 'footer.php';
