2ca68abb47f7582ca5795e40e2e2b22a5f9475d8
ASIR <ASIR@PC5041>
2018-12-18 13:01:02 +0100

    Es el primer commit

---
+++ RealizarCopia.php
@@ -0,0 +1,24 @@
+<?php
+
+echo '<form action = "RealizarCopia.php" method = "POST">
+    <table>
+    <tr>
+        <td>
+            <input type="radio" name="rbCopia" value="0" checked="checked" />Todas las BD
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <input type="radio" name="rbCopia" value="0" />Una sola BD
+        </td>
+        <td>
+            <input type="text" name="nombreBD" value="" size=""/>
+        </td>
+    </tr>
+    <tr>
+        <td>    
+            <input type="submit" value="Realizar" name="realizar" />
+        </td>
+    </tr>
+    </table>
+</form>';
